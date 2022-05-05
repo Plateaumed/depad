c975c7dbe376e32742537f6f06ae2185e7e8b031
Franklin Chieze <chieze.franklin@gmail.com>
2022-05-05 11:03:53 +0100

    set up basic cli

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,1 +1,25 @@
-export async function cli(): Promise<any> {}
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import fs from "fs-extra";
+import path from "path";
+import { showTitleAndBanner } from "./utils/logger";
+
+export async function cli(): Promise<any> {
+    showTitleAndBanner();
+
+    yargs(hideBin(process.argv)).command(
+            "version",
+            "Print the CLI and API version info",
+            (_yargs) => {
+                const packageJson = JSON.parse(
+                    fs.readFileSync(path.resolve(__dirname, "../package.json"), {
+                        encoding: "utf8",
+                        flag: "r",
+                    })
+                );
+                console.log(packageJson.version)
+
+                // server version
+            }
+        ).parse();
+}
--- packages/cli/src/utils/logger.ts
+++ packages/cli/src/utils/logger.ts
@@ -1,17 +1,22 @@
-import boxen, { Options } from "boxen";
-import chalk from "chalk";
+import { red, green, cyan } from "kleur";
+import figlet from "figlet";
 import { ConsoleMessage } from "./console-message";
 
 export const showTitleAndBanner = (): void => {
-    const boxenOptions: Options = {
-        padding: 1,
-        margin: 1,
-        borderStyle: "round",
-        borderColor: "green",
-        backgroundColor: "#555555"
-    };
-    const title = chalk.cyan.bold(ConsoleMessage.TITLE);
-    const titleBox = boxen( title, boxenOptions );
+    // const boxenOptions: Options = {
+    //     padding: 1,
+    //     margin: 1,
+    //     borderStyle: "round",
+    //     borderColor: "green",
+    //     backgroundColor: "#555555"
+    // };
+    // const title = chalk.cyan.bold(ConsoleMessage.TITLE);
+    // const titleBox = boxen( title, boxenOptions );
 
-    console.log(titleBox);
+    // console.log(ConsoleMessage.TITLE);
+
+    console.log(
+        cyan(figlet.textSync(ConsoleMessage.TITLE, { horizontalLayout: "full" }))
+    );
+    console.info(cyan(ConsoleMessage.BANNER));
 }
